3650bbd03bf3e065b63e57167423c22881bb4e8d
wanglele0410@sina.cn <wanglele0410@sina.cn>
2022-06-19 23:27:28 +0800

    update django

--- weblog/blog/views.py
+++ weblog/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, DetailView, ListView
+from django import views
 
 # Create your views here.
+
+class IndexTemplateView(TemplateView):
+    template_name = 'index.html'
